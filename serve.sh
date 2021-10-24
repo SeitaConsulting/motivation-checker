7092ab7dbb8090da35f95c968af89ff99cdc6786
Ubuntu <ubuntu@ip-172-31-42-177.ap-northeast-1.compute.internal>
2021-10-25 00:38:02 +0900

    first commit

---
+++ serve.sh
@@ -0,0 +1,1 @@
+sudo ng serve --port=80 --host=0.0.0.0
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'motivation-checker';
+  private _processing: boolean = false;
+  private _result = null;
+  private _tmp = null;
+
+  get processing(): boolean {
+    return this._processing;
+  }
+
+  get result(): number {
+    return this._result;
+  }
+
+  onSubmit(form): void {
+    this.start();
+
+    let value = form.value;
+    if(value.expectacy == 0) value.expectacy = 0.0000000000000000000001;
+    if(value.value == 0) value.value = 0.000000000000000000000000000001;
+    value.impulsivity = 100 - value.impulsivity;
+
+    this._tmp = (value.expectacy*value.value)/(value.impulsivity*value.delay);
+
+    this.finish();
+  }
+
+  reset(): void {
+    this._result = null;
+    this._tmp = null;
+  }
+
+  start(): void {
+    this._processing = true;
+  }
+
+  finish(): void {
+    const time = 1000;
+    const o = of('').pipe(delay(time));
+    o.subscribe(() => {
+      this._processing = false;
+      this._result = this._tmp;
+      console.log(this._result);
+    });
+  }
 }
